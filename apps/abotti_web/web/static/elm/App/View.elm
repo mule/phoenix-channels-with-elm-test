adc3fe0cb9e1195ec321271e81626bd55241121d
Jukka Puranen <jukka.puranen@gmail.com>
2016-09-17 00:34:53 +0300

    Setting up elm app

--- apps/abotti_web/web/static/elm/App/View.elm
+++ apps/abotti_web/web/static/elm/App/View.elm
@@ -3,7 +3,21 @@ module App.View exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import App.Update exposing (Msg, Model)
-
+import LayoutTemplates.Master as Layout
 view : Model -> Html Msg
 view model =
-    div [] [text "Hello from real elm app"]
+    div [class "cointainer-fluid"]  [Layout.view [] [mainView model] [agentBar model]] 
+
+mainView : Model -> Html Msg
+mainView model =
+    div [ class "main" ] [ text "Main view"]
+
+
+agentBar : Model -> Html Msg
+agentBar model =
+    ul [ class "agents" ] [ li [] [text "Appelsiini" ]]
+
+
+
+
+
---
+++ apps/abotti_web/web/static/elm/LayoutTemplates/Master.elm
@@ -0,0 +1,11 @@
+module LayoutTemplates.Master exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+view : List (Html a) -> List (Html a)  -> List (Html a) -> Html a  
+
+view left center right =
+     div [ class "row" ]
+                [ div [ class "col-xs-1" ] left
+                , div [ class "col-xs-9" ] center
+                , div [ class "co-xs-2" ] right
+                ]
